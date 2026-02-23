a2153242b22719e5974dcf20c460d21c4e4406e0
Emmanuel Raju <eraj007@gmail.com>
2026-02-24 02:32:36 +0530

    chore: completes tech task for iagon

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,58 @@
+<script>
+	import { backIn, backOut } from 'svelte/easing';
+
+	let { label, href, id, activeLink = $bindable() } = $props();
+
+	let isActive = $derived(activeLink === id);
+
+	function slideSpringIn(node, { duration = 300 } = {}) {
+		return {
+			duration,
+			css: (t) => {
+				const eased = backOut(t);
+				return `
+					transform: translateY(${(1 - eased) * 100}%);
+				`;
+			}
+		};
+	}
+	function slideSpringOut(node, { duration = 300 } = {}) {
+		return {
+			duration,
+			css: (t) => {
+				const eased = backIn(t);
+				const scaleEased = backOut(t * 0.25);
+				return `
+					transform: translateY(${(1 - eased) * 100}%) scaleX(${1 - scaleEased});
+					transform-origin: center;
+				`;
+			}
+		};
+	}
+</script>
+
+<a
+	{href}
+	class="relative flex flex-col overflow-hidden p-10 transition-colors duration-200 hover:text-blue-500 {isActive
+		? 'text-blue-500'
+		: 'text-white/50'}"
+	onclick={(e) => {
+		e.preventDefault();
+		activeLink = id;
+	}}
+>
+	<span>{label}</span>
+
+	{#if isActive}
+		<svg
+			class="svg-slide absolute -bottom-0.75 left-0 h-[25%] w-full transition duration-200"
+			in:slideSpringIn
+			out:slideSpringOut
+			viewBox="0 0 100 14"
+			preserveAspectRatio="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path d="M 0 14 C 20 14 30 1 50 1 C 70 1 80 14 100 14" fill="#fff" />
+		</svg>
+	{/if}
+</a>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Link from '$lib/components/Link.svelte';
+	let links = [
+		{
+			id: 1,
+			label: 'Menu 1',
+			href: '#Menu1'
+		},
+		{
+			id: 2,
+			label: 'Menu 2',
+			href: '#Menu2'
+		}
+	];
+
+	let activeLink = $state(1);
+</script>
+
+<svelte:head>
+	<title>IAGON | Tech Task</title>
+</svelte:head>
+
+<main class="grid min-h-screen place-content-center bg-black font-display">
+	<div class="overflow-hidden rounded-4xl border border-white/50 bg-black">
+		<nav class="flex justify-end gap-5 px-5">
+			{#each links as link (link.id)}
+				<Link {...link} bind:activeLink></Link>
+			{/each}
+		</nav>
+
+		<div class="h-[50vh] min-w-[60vw] rounded-t-4xl bg-white"></div>
+	</div>
+</main>
